bfae6ba9ab9d92bf35f3c5b86e0f99485998f943
trinity <agyemanjoseph12@gmail.com>
2022-02-08 23:00:49 +0000

    heroku

--- wives/settings/base.py
+++ wives/settings/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+# Activate Django-Heroku.
+import django_heroku
+
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -168,3 +171,5 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
